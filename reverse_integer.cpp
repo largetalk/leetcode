cd75f7307cca163c6b7e94751a3a8470db2cd534
Arthur <arthur_wang@adsame.com>
2014-10-28 00:08:44 +0800

    reverse integer

---
+++ reverse_integer.cpp
@@ -0,0 +1,17 @@
+
+//Reverse digits of an integer.
+//
+//Example1: x = 123, return 321
+//Example2: x = -123, return -321
+
+class Solution {
+    public:
+        int reverse(int x) {
+            int y = 0;
+            for (int i = 0; x != 0; i++) {
+                y = (y + x%10) * 10;
+                x /= 10;
+            }
+            return y/10;
+        }
+};
